ae0ea133e9135a0ab4fb0e97cdc7d5de33bf01dc
Miloš PeškanPeškan <milospeskanit523@gmail.com>
2025-08-23 21:54:25 +0200

    created data.js, moved roles from index.js to data.js

---
+++ data.js
@@ -0,0 +1,122 @@
+export const roles = {
+  1: { 
+    role: "Detektiv", 
+    alignment: "Selo", 
+    category: "Istraživačka", 
+    description: "Tokom noći može da otkrije ulogu jednog igrača." 
+  },
+  2: { 
+    role: "Doktor", 
+    alignment: "Selo", 
+    category: "Zaštitna", 
+    description: "Tokom noći može da zaštiti jednog igrača od eliminacije." 
+  },
+  3: { 
+    role: "Seljak", 
+    alignment: "Selo", 
+    category: "Obična", 
+    description: "Nema posebne moći, učestvuje samo u diskusiji i glasanju." 
+  },
+  4: { 
+    role: "Mafijaš", 
+    alignment: "Mafija", 
+    category: "Ubilačka", 
+    description: "Svake noći, zajedno sa mafijom, bira žrtvu za eliminaciju." 
+  },
+  5: { 
+    role: "Kum", 
+    alignment: "Mafija", 
+    category: "Ubilačka", 
+    description: "Vođa mafije, bira konačnu metu. Može izgledati nevino pri ispitivanju detektiva." 
+  },
+  6: { 
+    role: "Savetnik", 
+    alignment: "Mafija", 
+    category: "Istraživačka", 
+    description: "Može istražiti igrače i saznati njihovu tačnu ulogu." 
+  },
+  7: { 
+    role: "Pratilac", 
+    alignment: "Mafija", 
+    category: "Pomoćna", 
+    description: "Može blokirati jednog igrača svake noći i sprečiti ga da koristi moć." 
+  },
+  8: { 
+    role: "Serijski ubica", 
+    alignment: "Neutralno", 
+    category: "Ubilačka", 
+    description: "Samostalno ubija jednog igrača svake noći. Njegov cilj je da bude poslednji preživeli." 
+  },
+  9: { 
+    role: "Ludak", 
+    alignment: "Neutralno", 
+    category: "Haotična", 
+    description: "Želi da ga Selo pogrešno osudi na vešala tokom glasanja." 
+  },
+  10: { 
+    role: "Egzekutor", 
+    alignment: "Neutralno", 
+    category: "Haotična", 
+    description: "Njegov cilj je da navede Selo da osudi određenog igrača na vešala." 
+  },
+  11: { 
+    role: "Veštica", 
+    alignment: "Neutralno", 
+    category: "Kontrolna", 
+    description: "Može kontrolisati poteze drugih igrača i preusmeravati njihove radnje." 
+  },
+  12: { 
+    role: "Piroman", 
+    alignment: "Neutralno", 
+    category: "Ubilačka", 
+    description: "Može polivati igrače benzinom i u jednoj noći ih sve zapaliti." 
+  },
+  13: { 
+    role: "Telohranitelj", 
+    alignment: "Selo", 
+    category: "Zaštitna", 
+    description: "Može čuvati jednog igrača tokom noći i žrtvovati se ako ga napadnu." 
+  },
+  14: { 
+    role: "Šerif", 
+    alignment: "Selo", 
+    category: "Istraživačka", 
+    description: "Ispituje igrače tokom noći i saznaje da li su sumnjivi (Mafija ili neutralni ubica)." 
+  },
+  15: { 
+    role: "Špijun", 
+    alignment: "Selo", 
+    category: "Istraživačka", 
+    description: "Saznaje koga će Mafija napasti tokom noći." 
+  },
+  16: { 
+    role: "Tragač", 
+    alignment: "Selo", 
+    category: "Istraživačka", 
+    description: "Može pratiti jednog igrača i saznati koga je posetio te noći." 
+  },
+  17: { 
+    role: "Medium", 
+    alignment: "Selo", 
+    category: "Pomoćna", 
+    description: "Može komunicirati sa mrtvim igračima tokom noći." 
+  },
+  18: { 
+    role: "Eskort", 
+    alignment: "Selo", 
+    category: "Pomoćna", 
+    description: "Može blokirati jednog igrača svake noći i sprečiti ga da koristi moć." 
+  },
+  19: { 
+    role: "Grobar", 
+    alignment: "Selo", 
+    category: "Istraživačka", 
+    description: "Tokom noći može da sazna ulogu osobe koja je najskorije ubijena." 
+  },
+  20: { 
+    role: "Amnezičar", 
+    alignment: "Neutralno", 
+    category: "Fleksibilna", 
+    description: "Može tokom igre odabrati da preuzme ulogu nekog već eliminisanog igrača." 
+  }
+};
